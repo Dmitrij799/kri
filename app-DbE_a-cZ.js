559e64e6daefe07b83c14f030b53bcb4d0bbbac8
Drino955 <dima.nikiforov.2017.2@gmail.com>
2025-03-22 17:37:55 +0100

    fix: protocol version

--- app-DbE_a-cZ.js
+++ app-DbE_a-cZ.js
@@ -149,7 +149,7 @@ const GameConfig = {
   // the protocol we originated from was 78
   // remember to bump this every time a serialization function is changed
   // or a definition item added, removed or moved
-  protocolVersion: 1e3,
+  protocolVersion: 1001,
   Input,
   EmoteSlot,
   WeaponSlot,
